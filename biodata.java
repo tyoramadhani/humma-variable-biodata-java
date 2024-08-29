012f74628aca55db8192e7845e4688628cd8c273
tyoramadhani <tyoramadani5@gmail.com>
2024-08-29 19:59:29 +0700

    first commit

---
+++ biodata.java
@@ -0,0 +1,30 @@
+public class biodata {
+    public static void main(String[] args) {
+        String fullName = "Dwi Setyo Ramadhani";
+        System.out.println("Nama Saya Adalah : " + fullName);
+
+        String nickName = "Tyo";
+        System.out.println("Bisa Di Panggil : " + nickName);
+
+        int age = 17;
+        System.out.println("Umur : " + age + " Tahun");
+
+        float height = 175.5f;
+        System.out.println("Tinggi Badan Saya : " + height + " Cm");
+
+        double weight = 75.8;
+        System.out.println("Berat Badan Saya : " + weight + " Kg");
+
+        String cityOfBirth = "Tuban";
+        System.out.println("Saya Lahir Di :" + cityOfBirth);
+
+        char gender = 'L';
+        System.out.println("Jenis Kelamin : " + gender + "aki-laki");
+
+        boolean isMarried = false;
+        System.out.println("Sudah Menikah? : " + isMarried);
+
+        boolean isWNI = true;
+        System.out.println("Saya WNI? : " + isWNI);
+    }
+}
